41881d8f519064097dcef5ecfa0c3186d7729811
Jake <jake@jakebills.com>
2019-11-10 14:19:40 -0700

    added ComicBook and Artist models

---
+++ comic-book-gallery/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comic_book_gallery.Models
+{
+    public class Artist
+    {
+        public string Role { get; set; }
+        public string Name { get; set; }
+    }
+}
