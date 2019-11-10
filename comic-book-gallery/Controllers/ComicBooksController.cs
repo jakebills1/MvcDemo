ea60907c0b3e6c3cefc3f1d8d91deeb93a6cbfae
Jake <jake@jakebills.com>
2019-11-10 12:34:55 -0700

    adds new controller and view

--- comic-book-gallery/Controllers/ComicBooksController.cs
+++ comic-book-gallery/Controllers/ComicBooksController.cs
@@ -10,9 +10,20 @@ namespace comic_book_gallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "hello world";
+            ViewBag.SeriesTitle = "the amazing spider man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+            return View();
         }
     }
 }
