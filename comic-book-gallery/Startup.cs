509ed3ef1fc7e0c84fa0e31c28b97046c6658ff2
Jake <jake@jakebills.com>
2019-11-11 15:03:30 -0700

    adds routing to index page

--- comic-book-gallery/Startup.cs
+++ comic-book-gallery/Startup.cs
@@ -50,7 +50,7 @@ namespace comic_book_gallery
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=ComicBooks}/{action=Index}/{id?}");
             });
         }
     }
